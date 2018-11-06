eed0018ce0e94e0a466c9b142fe5826da91ca016
liu,yibei <lebniz@gmail.com>
2018-11-06 16:11:48 +0900

    fixed

---
+++ js/comment.js
@@ -0,0 +1,50 @@
+var APP_ID = 'dbkqgkS5KaiLREIKUppiXSVI-gzGzoHsz';
+var APP_KEY = '0H0TNESh9J8qHPU2xDHisgld';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
+
+var query = new AV.Query('Message');
+query.find()
+    .then(
+        function(messages) {
+            let array = messages.map((item) => item.attributes)
+            array.forEach((item) => {
+                let li = document.createElement('li')
+                li.innerText = `${item.name}: ${item.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
+            })
+        }
+    )
+
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let content = myForm.querySelector('input[name=content]').value
+    let name = myForm.querySelector('input[name=name]').value
+    var Message = AV.Object.extend('Message');
+    var message = new Message();
+    message.save({
+        'name':name,
+        'content':content
+    }).then(function(object) { //obiect为存入的数据的相关信息
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}:${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value=''
+    })
+})
